8d728cd7c3c497117f5b1ec44ef7f8e23006c00e
Angel Ivanov <a_ivanovvv@abv.bg>
2017-05-08 23:22:35 +0300

    last update added

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var autoPrefixer = require('gulp-autoprefixer');
 var clean = require('gulp-clean');
 var concat = require('gulp-concat');
 var browserify = require('gulp-browserify');
+var merge = require('merge-stream');
 
 // Object for source folder paths
 var SOURCEPATHS = {
@@ -37,9 +38,13 @@ gulp.task('clean-scripts', function() {
 
 // 1. Gulp sass - This task is for compile scss files to css files
 gulp.task('sass', function() {
-    return gulp.src(SOURCEPATHS.sassSource)
+    var bootstrapCSS = gulp.src('./node_modules/bootstrap/dist/css/bootstrap.css');
+    var sassFiles;
+    sassFiles = gulp.src(SOURCEPATHS.sassSource)
         .pipe(autoPrefixer())
-        .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+        .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError));
+        return merge(bootstrapCSS, sassFiles)
+        .pipe(concat('app.css'))
         .pipe(gulp.dest(APPPATH.css));
 });
 // 2. Copy task - this task is for creating copies of html files form srs folder to app folder.
@@ -48,13 +53,13 @@ gulp.task('scripts', ['clean-scripts'], function() {
         .pipe(concat('main.js'))
         .pipe(browserify())
         .pipe(gulp.dest(APPPATH.js));
-})
+});
 
 // 3. Copy task - this task is for creating copies of html files form srs folder to app folder.
 gulp.task('copy', ['clean-html'], function() {
     gulp.src(SOURCEPATHS.htmlSource)
         .pipe(gulp.dest(APPPATH.root));
-})
+});
 
 // 4. Browser sync task - this task is for automaticly update all changes in css, html and js files. Also create a localhost path and automaticly added in browser.
 gulp.task('serve', ['sass'], function() {
@@ -62,7 +67,7 @@ gulp.task('serve', ['sass'], function() {
         server: {
             baseDir: APPPATH.root
         }
-    })
+    });
 });
 
 // 5. Gulp watch task - this task is for looking if there any changes in scss or js files of html files. If there are the watch task automaticly updated and refreshed in browser.
@@ -70,7 +75,7 @@ gulp.task('watch', ['serve', 'sass', 'copy', 'clean-html', 'clean-scripts', 'scr
     gulp.watch([SOURCEPATHS.sassSource], ['sass']);
     gulp.watch([SOURCEPATHS.htmlSource], ['copy']);
     gulp.watch([SOURCEPATHS.jsSource], ['scripts']);
-})
+});
 
 // N. Gulp default task
 gulp.task('default', ['watch']);
